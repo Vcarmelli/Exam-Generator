55668d15f2f7090f29c3482e97a1ec04f8188b5f
Vcarmelli <vashti.karmelli16@gmail.com>
2024-10-01 18:55:01 +0800

    fix css multiple choice

--- ExamGenerator/game.py
+++ ExamGenerator/game.py
@@ -4,12 +4,12 @@ game = Blueprint('game', __name__, url_prefix='/game')
 
 @game.route('/identification')
 def identification():
-    return render_template('game/identification.html')
+    return render_template('game/identification.html', game_mode='Identification')
 
 @game.route('/multiple-choice')
 def multiple_choice():
-    return render_template('game/multiple-choice.html')
+    return render_template('game/multiple-choice.html', game_mode='Multiple Choice')
 
 @game.route('/true-or-false')
 def true_or_false():
-    return render_template('game/true-or-false.html')
+    return render_template('game/true-or-false.html', game_mode='True or False')
